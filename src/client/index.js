085fde45e22cb0b987a506bdbf9dbb4d39b2c4bd
Hosen Poursaeid <poursaeid@outlook.com>
2018-07-25 06:43:38 -0400

    setup flow

---
+++ src/client/index.js
@@ -0,0 +1,28 @@
+import FormContainer from './js/components/container/FormContainer';
+
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked this.';
+    }
+
+    return e(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Like'
+    );
+  }
+}
+
+const domContainer = document.querySelector('#app');
+ReactDOM.render(e(LikeButton), domContainer);
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const CleanWebPackPlugin = require('clean-webpack-plugin');
+
+const outputDirectory = 'dist';
+module.exports = {
+  entry: './src/client/index.js',
+  output: {
+    path: path.join(__dirname, outputDirectory),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader'
+          }
+        ]
+      }
+    ]
+  },
+  devServer: {
+    port: 3000,
+    open: true,
+    proxy: { '/api': 'http://localhost:8080' }
+  },
+  plugins: [
+    new CleanWebPackPlugin([outputDirectory]),
+    new HtmlWebPackPlugin({
+      template: './public/index.html',
+      favicon: './public/favicon.ico'
+    })
+  ]
+};
